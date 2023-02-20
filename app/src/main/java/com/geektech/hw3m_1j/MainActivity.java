8385289fa12dc2fe0e2855beeeeb0d81c3dfe55a
Indira <indirakambarova08@gmail.com>
2023-02-20 14:42:52 +0600

    Initial commit

---
+++ app/src/main/java/com/geektech/hw3m_1j/MainActivity.java
@@ -0,0 +1,35 @@
+package com.geektech.hw3m_1j;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.edt_view);
+        button = findViewById(R.id.btn_open);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,editText.getText().toString());
+                sendIntent.setType("text/plain");
+                startActivity(sendIntent);
+            }
+        });
+    }
+}
